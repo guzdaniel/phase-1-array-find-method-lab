ccd9e1e9397178bb6a55fa5568b54bff36b681e2
Daniel Guzman <guzdan@live.com>
2021-12-05 20:03:40 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "L"},
+    {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(teamRecord){
+    const winElement = teamRecord.find(compareWin)
+
+    if(winElement !== undefined){
+        return winElement.year
+    }
+    else{
+        return winElement
+    }
+    
+}
+
+function compareWin(element){
+    return element.result === "W"
+}
+
+
+
